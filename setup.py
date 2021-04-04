985299f0c5fd4a958cb1edd0e91d668a550a719e
Peter Schmidbauer <peter.schmidb@gmail.com>
2021-04-04 21:34:44 +0200

    Bump version to 2021.04.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fp:
 
 setup(
     name="statement-dl",
-    version="2021.02.1",
+    version="2021.04.1",
     description="Automatic download of banking/broker documents from flatex",
     long_description=long_description,
     long_description_content_type="text/markdown",
